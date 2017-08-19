1bff7be00db7f5cc7e374b7f9a415688a5403e54
Coral <coral00685@gmail.com>
2017-08-19 16:24:26 +0800

    20170819

---
+++ app/src/main/java/tw/jason/app/helloworld/myonbootservice/MyReceiver.java
@@ -0,0 +1,17 @@
+package tw.jason.app.helloworld.myonbootservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("brad","onReceive");
+        Intent it = new Intent(context,MyService.class);
+        context.startService(it);
+    }
+
+}
---
+++ app/src/main/java/tw/jason/app/helloworld/myonbootservice/MyService.java
@@ -0,0 +1,69 @@
+package tw.jason.app.helloworld.myonbootservice;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+    private Timer timer;
+    private NotificationManager nmgr;
+    private int i;
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return  null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        nmgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        timer = new Timer();
+        timer.schedule(new MyTask(),0,10*1000);
+    }
+
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            //Log.i("brad","dosomething");
+            sendNotice();
+        }
+    }
+    private  void sendNotice(){
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setTicker("XXX");
+        builder.setLights(Color.argb(255,0,0,255),200,500);
+        builder.setContentTitle("title"+ i++);
+        builder.setContentText("content");
+        builder.setAutoCancel(true);
+
+        Intent it = new Intent(this,NoticeActivity.class);
+        it.putExtra("key",i);
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addParentStack(NoticeActivity.class);
+        taskStackBuilder.addNextIntent(it);
+
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent);
+
+        Notification notification = builder.build();
+
+        nmgr.notify(1,notification);
+
+    }
+}
